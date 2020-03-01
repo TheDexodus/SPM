b9018b08fbd6f8a5d5f1b911429aac41a0c6b151
Dexodus <daniilarosuk9@gmail.com>
2020-03-01 22:25:44 +0300

    init project

---
+++ page/views/admin-article/_form.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\page\models\Article */
+/* @var $form yii\widgets\ActiveForm */
+/* @var $categories */
+/* @var $tags */
+?>
+
+<div class="static-page-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'author')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'slug')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'short_content')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'rating')->textInput() ?>
+
+    <?= $form->field($model, 'category')->dropDownList($categories) ?>
+
+    <?= $form->field($model, 'tags')->dropDownList($tags, ['multiple' => 'multiple']) ?>
+
+    <?= $form->field($model, 'status')->dropDownList($model->getStatusList()) ?>
+
+    <?= $form->field($model, 'link')->textInput(['value' => $model->link ?? sha1(microtime())]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
